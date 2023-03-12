e3dd73cfe62123cf6461ebec51dfb264edd4cdfe
Sayan Ghosh <69137771+Shadow129-sys@users.noreply.github.com>
2023-03-12 17:14:07 +0530

    Update prefix_automata.cpp

--- prefix_automata.cpp
+++ prefix_automata.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 using vi = vector<int>;
-const int mxa = 2;
+const int mxa = 26;
 
 vi prefix_function(string &S){
 	int n = S.length();
@@ -18,10 +18,24 @@ vi prefix_function(string &S){
 
 vector<vi> prefix_automata(string &S){
 	int n = S.length();
+	
+	// Uses KMP to get the pi values
 	vi pi = prefix_function(S);
+	
+	// The declaring graph/automata
 	vector<vi> automata(n+1, vi(mxa, -1));
+	
+	// Initialization
 	for(int i=0;i<mxa;i++)automata[0][i] = 0;
 	automata[0][S[0]-'a'] = 1;
+	
+	// Creating the automata using the value from prefix function in O(26.N) time.
+	// Graph indices are from 0 to n.
+	// String indices are from 0 to n-1.
+	/* 
+	node 0 is the starting vertex, for staring matching we always start from node 0 and follow the edge according to the values 
+	in the given string i.e. not the pattern string. the max value of c will give us the max prefix of pattern string that matches with given string.
+	*/
 	for(int i=0;i<n;i++){
 		for(int c=0;c<mxa;c++){
 			if((i+1)<n && S[i+1]==(c+'a'))automata[i+1][c] = i+2;
